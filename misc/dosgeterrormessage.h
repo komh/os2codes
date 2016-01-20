cc5831332ce9cfc5e21fe3ba810ff32f29a8b9f7
KO Myung-Hun <komh@chollian.net>
2016-01-20 19:46:12 +0900

    dosgeterrormessage: add brief documentation
        modified:   misc/dosgeterrormessage.h

--- misc/dosgeterrormessage.h
+++ misc/dosgeterrormessage.h
@@ -10,6 +10,8 @@
  * http://www.wtfpl.net/ for more details.
  */
 
+/** @file dosgeterrormessage.h */
+
 #ifndef OS2CODES_DOSGETERRORMESSAGE_H
 #define OS2CODES_DOSGETERRORMESSAGE_H
 
@@ -19,6 +21,13 @@
 extern "C" {
 #endif
 
+/**
+ * @brief Get an error message from an error code, @a errnum
+ * @param[in] errnum Error code
+ * @param[out] buffer Place to store an error message
+ * @param[in] n Size of the buffer pointed by @a buffer in bytes
+ * @return @a buffer on success, otherwise NULL
+ */
 const char *DosGetErrorMessage( int errnum, char *buffer, size_t n );
 
 #ifdef __cplusplus
