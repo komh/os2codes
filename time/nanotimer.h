8128d0d41f4d73c240beb62a5be75d893539556a
KO Myung-Hun <komh@chollian.net>
2016-01-20 20:17:35 +0900

    nanotimer: add brief documentation
        modified:   time/nanotimer.h

--- time/nanotimer.h
+++ time/nanotimer.h
@@ -10,22 +10,37 @@
  * http://www.wtfpl.net/ for more details.
  */
 
+/** @file nanotimer.h */
+
 #ifndef OS2CODES_NANOTIMER_H
 #define OS2CODES_NANOTIMER_H
 
+/**
+ * @brief Class implementing nano timer
+ */
 class NanoTimer
 {
 public:
+    /** Constructor */
     NanoTimer();
+    /** Destructor */
     ~NanoTimer();
 
+    /** NanoClock type */
     typedef long long int NanoClock;
 
+    /** 1 second in nano seconds */
     static const NanoClock NANO_TIME_BASE = 1000LL * 1000LL * 1000LL;
 
+    /**
+     * @brief Get a nano clock snapshot from IRQ0 high resolution timer
+     * (Intel 8254)
+     * @return A nano clock snapshot
+     */
     NanoClock clock();
 
 private:
+    /** Frequency of IRQ0 high resolution timer (Intel 8254) */
     unsigned long _freq;
 };
 #endif
